b0fb14b2f58b533e51997752f094ef18c187bb91
PontusIK <pontus.kavrestad@gmail.com>
2025-09-26 15:02:24 +0000

    added new game button and empty functions for button presses

--- src/ui.py
+++ src/ui.py
@@ -1,6 +1,15 @@
 import tkinter as tk
 from game import Game
 
+def onHit():
+    pass
+
+def onStand():
+    pass
+
+def onStart():
+    pass
+
 def init():
     root = tk.Tk()
     root.title("Blackjack")
@@ -11,9 +20,11 @@ def init():
     controlsFrame = tk.Frame(root)
     controlsFrame.pack(fill="x")
 
-    hitButton = tk.Button(controlsFrame, text="Hit", width=10)
+    hitButton = tk.Button(controlsFrame, text="Hit", width=10, command=onHit)
     hitButton.pack(side="right", pady=10, padx=10)
-    standButton = tk.Button(controlsFrame, text="Stand", width=10)
+    standButton = tk.Button(controlsFrame, text="Stand", width=10, command=onStand)
     standButton.pack(side="right", pady=10, padx=10)
+    startButton = tk.Button(controlsFrame, text="New Game", width=10, command=onStart)
+    startButton.pack(side="left", pady=10, padx=10)
 
     root.mainloop()
